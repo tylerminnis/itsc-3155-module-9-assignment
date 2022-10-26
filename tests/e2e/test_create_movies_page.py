6d46f956a45092e437f01cf3fb55789d78721492
Nathan Laney <nathaniel.laney@gmail.com>
2022-10-25 21:37:49 -0400

    Complete E2E tests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,9 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+
+
+def test_home_page(test_app: FlaskClient):
+    response = test_app.get('/test_create_movies_page.py')
+    response_data = response.data
+
+    assert b'<h1 class="mb-5">Create Movie Rating</h1>' in response_data
